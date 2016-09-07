373cf003c587e072ce859c87ca5d76aa02896307
Pietro <donapieppo@yahoo.it>
2016-09-07 18:10:06 +0200

    validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,9 @@ class Event < ApplicationRecord
   belongs_to :wall
   has_many :photos
 
+  validates :name, presence: true
+  validates :start_date, presence: true
+
   def to_s
     I18n.l(self.start_date) + " " + self.name
   end
