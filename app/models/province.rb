ba692a8a124e8bea93b4c14ab93894ca06b4d703
Pietro Donatini <pietro.donatini@unibo.it>
2016-09-21 21:45:32 +0200

    cache

--- app/models/province.rb
+++ app/models/province.rb
@@ -2,5 +2,12 @@ class Province < ApplicationRecord
   belongs_to :region
   has_many :walls
 
+  # cache, non cambiano mai
+  @@provinces_json = Province.order(:name).select(:name).pluck(:name).to_json
+
+  def self.provinces_json
+    @@provinces_json
+  end
+
 end
 
