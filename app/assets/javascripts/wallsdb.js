51f285b5406a9ddcff3b16e3cee4df8ff90f84a8
Pietro Donatini <pietro.donatini@unibo.it>
2016-09-21 17:07:00 +0200

    cliccable event

--- app/assets/javascripts/wallsdb.js
+++ app/assets/javascripts/wallsdb.js
@@ -1,4 +1,7 @@
 $(function() {
   $("input.datetime_picker").datetimepicker({locale: 'it', stepping: 15, sideBySide: true});
+  $(".event-container").click( function() {
+    window.location.href = $(this).data("url");
+  });
 });
 
