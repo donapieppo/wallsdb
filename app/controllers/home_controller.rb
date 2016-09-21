25dc8498665ee98316abcb3ce0f17e6651efcc7f
Pietro Donatini <pietro.donatini@unibo.it>
2016-09-21 21:44:40 +0200

    index with province

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,13 @@ class HomeController < ApplicationController
   skip_before_action :force_sso_user
 
   def index
-    @home_header = true
+    @home_header    = true
     @search_in_menu = true
-    @walls = Wall.includes(province: :region).order('regions.name')
+    @walls  = Wall.includes(province: :region).order('regions.name')
+    if params[:province] 
+      @province = Province.where(name: params[:province]).first
+      @walls = @walls.where(province_id: @province.id) if @province
+    end
     @events = Event.includes(:photos).order(:start_date).limit(10)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :photos
     resources :events
   end
+
   resources :admins
   resources :openings
   resources :photos
@@ -14,15 +15,18 @@ Rails.application.routes.draw do
     resources :photos
   end
 
+  get  'pippo', to: 'home#pippo'
   post 'show_province', to: 'home#show_province', as: :show_province
 
   get 'login', to: 'logins#index', as: :login
   # get 'login_facebook', to: 'logins#facebook', as: :facebook_login
  
   get 'logins/logout',               to: 'logins#logout',    as: :logout
-  get 'auth/google_oauth2/callback', to: 'logins#google_oauth2'
   get 'auth/google_oauth2',          as: 'google_login'
+  get 'auth/google_oauth2/callback', to: 'logins#google_oauth2'
   get 'logins/no_access',            to: 'logins#no_access', as: :no_access
 
   post 'auth/developer/callback',    to: 'logins#developer'
+
+  get '/(:province)', to: 'home#index'
 end
