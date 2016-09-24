3cb194306a709eecd43892efefb55b0866e8953c
donapieppo <donapieppo@yahoo.it>
2016-09-24 16:56:35 +0200

    add lat/lng from address

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -36,6 +36,19 @@ class Wall < ApplicationRecord
     ! self.music.blank?
   end
 
+  def fill_geocodes
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{gmap_address},+IT&key=#{ENV['WALLSDB_GOOGLE_MAP_API_KEY']}"
+    response = ActiveSupport::JSON.decode(Net::HTTP.get(URI.parse(url)))
+    if response["status"] == "OK"
+      res = response["results"]
+      # "geometry"=>{"location"=>{"lat"=>44.5149894, "lng"=>11.3592175}
+      lat_lng = res[0]["geometry"]["location"]
+      self.update_attributes(lat: lat_lng["lat"], lng: lat_lng["lng"])
+    else
+      self.errros.add(:address, message: "Indirizzo sconosciuto su google map")
+    end
+  end
+
   def self.addresses 
     self.includes(:province).map {|wall| "#{wall.address}, #{wall.city}, #{wall.province.name}"}
   end
@@ -46,4 +59,5 @@ class Wall < ApplicationRecord
 
 end
 
-
+#  => {"results"=>[{"address_components"=>[{"long_name"=>"59", "short_name"=>"59", "types"=>["street_number"]}, {"long_name"=>"Via Stalingrado", "short_name"=>"Via Stalingrado", "types"=>["route"]}, {"long_name"=>"Bologna", "short_name"=>"Bologna", "types"=>["locality", "political"]}, {"long_name"=>"Bologna", "short_name"=>"Bologna", "types"=>["administrative_area_level_3", "political"]}, {"long_name"=>"Città Metropolitana di Bologna", "short_name"=>"BO", "types"=>["administrative_area_level_2", "political"]}, {"long_name"=>"Emilia-Romagna", "short_name"=>"Emilia-Romagna", "types"=>["administrative_area_level_1", "political"]}, {"long_name"=>"Italy", "short_name"=>"IT", "types"=>["country", "political"]}, {"long_name"=>"40128", "short_name"=>"40128", "types"=>["postal_code"]}], "formatted_address"=>"Via Stalingrado, 59, 40128 Bologna, Italy", "geometry"=>{"location"=>{"lat"=>44.5149894, "lng"=>11.3592175}, "location_type"=>"ROOFTOP", "viewport"=>{"northeast"=>{"lat"=>44.51633838029149, "lng"=>11.3605664802915}, "southwest"=>{"lat"=>44.5136404197085, "lng"=>11.3578685197085}}}, "partial_match"=>true, "place_id"=>"ChIJL_11ClvTf0cRoto9Fx2UqD0", "types"=>["street_address"]}], "status"=>"OK"} 
+#
