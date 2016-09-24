fbf36c30898303a9ef81410aa0bb68adf1e5a268
donapieppo <donapieppo@yahoo.it>
2016-09-24 15:32:00 +0200

    refactor

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -5,10 +5,18 @@ class Opening < ApplicationRecord
 
   def to_s
     if self.wday
-      DAY_NAMES[self.wday] + " " + I18n.l(self.time_open, format: :hour) + " - " + I18n.l(self.time_close, format: :hour)
+      wday_to_s + " " + hours_to_s
     else
       "ERRORE"
     end
+  end
+
+  def wday_to_s
+    DAY_NAMES[self.wday]
+  end
+
+  def hours_to_s
+    I18n.l(self.time_open, format: :hour) + " - " + I18n.l(self.time_close, format: :hour)
   end
 end
 
