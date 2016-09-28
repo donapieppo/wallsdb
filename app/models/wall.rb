2a95ea2ee3b68d23fe79d9d2516ed14caae12d02
Pietro Donatini <pietro.donatini@unibo.it>
2016-09-28 23:31:23 +0200

    varie

--- app/models/wall.rb
+++ app/models/wall.rb
@@ -11,6 +11,11 @@ class Wall < ApplicationRecord
 
   before_save :fill_geocodes
 
+  # allowed_tags = Set.new(%w(strong em b i p code pre tt samp kbd var sub
+  #      sup dfn cite big small address hr br div span h1 h2 h3 h4 h5 h6 ul ol li dl dt dd abbr
+  #      acronym a img blockquote del ins))
+  SANITIZE = Rails::Html::WhiteListSanitizer.new
+
   def to_s
     self.name
   end
