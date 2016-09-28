f2a0b5fed61fd0e6096bbe340c7dd18258eb69ae
Pietro Donatini <pietro.donatini@unibo.it>
2016-09-28 23:24:57 +0200

    privacy

--- app/helpers/privacy_helper.rb
+++ app/helpers/privacy_helper.rb
@@ -10,7 +10,7 @@ module PrivacyHelper
     else
       cookies[name] = { value: "accepted", expires: 1.year.from_now }
       raw %Q|<div class="alert alert-warning alert-dismissible" role="alert">
-           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times; chiudi</span></button> Questo sito utilizza solo cookie tecnici per il corretto funzionamento delle pagine web e per il miglioramento dei servizi. Proseguendo la navigazione del sito o cliccando su "chiudi" acconsenti all'uso dei cookie.</div>|
+           <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times; chiudi</span></button> Questo sito web utilizza cookie analitici di terze parti. Utilizzando il sito si intende accettata la Cookie Policy.</div>|
     end
   end
 end
