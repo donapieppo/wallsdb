f61cd3f4499c983fe97e54a2d1df1ac087320925
donapieppo <donapieppo@yahoo.it>
2016-09-29 00:19:43 +0200

    facebook

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   get 'logins/logout',               to: 'logins#logout',    as: :logout
   get 'auth/google_oauth2',          as: 'google_login'
   get 'auth/google_oauth2/callback', to: 'logins#google_oauth2'
+  get 'auth/facebook',               as: 'facebook_login'
+  get 'auth/facebook/callback',      to: 'logins#facebook'
   get 'logins/no_access',            to: 'logins#no_access', as: :no_access
 
   post 'auth/developer/callback',    to: 'logins#developer'
