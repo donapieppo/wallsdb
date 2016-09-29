5c6f8c6c1084a69629b6520dd0aa2383c5f3bf05
donapieppo <donapieppo@yahoo.it>
2016-09-29 23:01:40 +0200

    style

--- app/helpers/walls_helper.rb
+++ app/helpers/walls_helper.rb
@@ -8,28 +8,30 @@ module WallsHelper
 
 
   def details_bar(wall)
-    content_tag :ul, class: "wall-circles" do
+    content_tag :div, class: "wall-details" do
       capture do
-        concat li_tag(wall, :boulder) 
-        concat li_tag(wall, :rope)
-        concat li_tag(wall, :training)
-        concat li_tag(wall, :bar)
-        concat li_tag(wall, :music)
+        concat wall_detail(wall, :boulder) 
+        concat wall_detail(wall, :rope)
+        concat wall_detail(wall, :training)
+        concat wall_detail(wall, :bar)
+        concat wall_detail(wall, :music)
       end
     end
   end
 
-  def li_tag(wall, what)
+  def wall_detail(wall, what)
     present = wall.send("has_#{what}?")        
     mq      = [:boulder, :rope].include?(what) ? wall.send("#{what}_mq")    : nil
     notes   = [:boulder, :rope].include?(what) ? wall.send("#{what}_notes") : wall.send(what)
 
-    popover = present ? { toggle: "popover", content: notes, container: "body", placement: "bottom"} : ''
-
-    content_tag :li, class: (present ? '' : 'absent'), data: popover, title: t(what) do 
-      icon(WallIcons[what], size: 38) +
-      "<br/>".html_safe +
-      (mq ? "#{mq} m.q. di #{t what}" : t(what))
+    content_tag :div, class: (present ? 'col ' : 'col absent')  do 
+      content_tag :div, class: 'wall-detail' do
+        capture do
+          content_tag('span') { icon(WallIcons[what], size: 28) } +
+          content_tag('h3') { (mq ? "#{mq} m.q. di #{t what}" : t(what)) } +
+          content_tag('p') { notes }
+        end
+      end
     end
   end
 
