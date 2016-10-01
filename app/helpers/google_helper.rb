4f1c87d91a4c01e546581729ce9f7e51a4582fac
Pietro Donatini <pietro.donatini@unibo.it>
2016-10-01 11:07:17 +0200

    error

--- app/helpers/google_helper.rb
+++ app/helpers/google_helper.rb
@@ -1,6 +1,6 @@
 module GoogleHelper
   def simple_gmap_url(address)
-    "//www.google.com/maps/embed/v1/place?key=#{ENV['WALLSDB_GOOGLE_MAP_API_KEY']}&q=#{h address}&scrollwheel=false".html_safe
+    "//www.google.com/maps/embed/v1/place?key=#{ENV['WALLSDB_GOOGLE_MAP_API_KEY']}&q=#{h address}".html_safe
   end
 end
 
