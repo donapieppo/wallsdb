ccae45e6f2524c7d64eaf44d0dd7a4f54c522399
Pietro <donapieppo@yahoo.it>
2017-02-02 13:21:23 +0100

    trying colors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,5 +1,9 @@
 module EventsHelper
-  COLORS = %w(#8bacbd #dbc19c #f75155)
+  COLORS = %w(#ffca28 #009688 #90a4ae)
+  # COLORS = %w(#dddfd4 #fae596 #3fb0ac )
+  # COLORS = %w( #dbe9d8 #c2d4d8 #f2efe8 #b0aac2 )
+  # COLORS = %w(#8ea9e8 #8d82c4 #e7b788)
+  # COLORS = %w(#8bacbd #dbc19c #f75155)
   def event_bg_color
     @i ||= -1
     COLORS[(@i += 1) % COLORS.size]
