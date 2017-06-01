234fa8b9ccfe3e0a7bc825f60dc6c1c47375cbbb
Donapieppo <donapieppo@yahoo.it>
2017-06-01 16:30:10 +0200

    rails 5.1.1

--- config/application.rb
+++ config/application.rb
@@ -6,17 +6,19 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-MASTERS_OF_UNIVERSE = ['luca@gmail.com',
-                       'donapieppo@gmail.com']
+MASTERS_OF_UNIVERSE = ['luca@gmail.com', 'donapieppo@gmail.com']
 
 module Wallsdb
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     config.time_zone = 'Rome'
     config.i18n.default_locale = :it
     config.active_record.time_zone_aware_types = [:datetime, :time]
 
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
